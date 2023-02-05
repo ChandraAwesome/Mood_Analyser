38fcf89cff16ab64930df4517d8836f9ce2b2533
ChandarAwesome <ajaygames00Gmail.com>
2023-02-05 13:00:13 +0530

    [Chandra]-MoodAnalyser

---
+++ MoodAnalyserDemo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analyser
+{
+    public class MoodAnalyserDemo
+    {
+        private string message;
+        public MoodAnalyserDemo(string message)
+        {
+            this.message = message;
+        }
+        public string analyseMood()
+        {
+            //"null"==""
+            // string s = null;
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+                if (this.message.Contains("sad"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mood_Analyser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
